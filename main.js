4d1153575f043c57478eb61d4208f4ed2ed26534
adkrueger <adkrueger@wpi.edu>
2020-07-28 08:08:16 -0400

    rearranged things, added more style

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function displaySection(isLong) {
   let longSection = document.getElementById("long_version");
+  let longButton = document.getElementById("long_button");
   let shortSection = document.getElementById("short_version");
+  let shortButton = document.getElementById("short_button");
 
   longSection.style.display = isLong ? "block" : "none";
+  longButton.style.fontWeight = isLong ? "bold" : "normal";
   shortSection.style.display = isLong ? "none" : "block";
+  shortButton.style.fontWeight = isLong ? "normal" : "bold";
 }
 
 function dropDown() {
